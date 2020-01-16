1a01a927614f04e181623f1c65f1a99f5f001603
LN PC <brunogirome@gmail.com>
2020-01-16 07:36:43 -0300

    Finalizaçao do main, +/- 1:05:00h de video!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './global.css'
+import './App.css'
+import './Sidebar.css'
+import './Main.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">
+            Salvar
+          </button>
+        </form>
+      </aside>
+
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/44439996?s=460&v=4" alt="avatar" />
+              <div className="user-info">
+                <strong>Bruno Girome</strong>
+                <span>Java, JavaScript, PHP</span>
+              </div>
+            </header>
+            <p>Bio do usuário.</p>
+            <a href="https://github.com/brunogirome">Acessar Perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/44439996?s=460&v=4" alt="avatar" />
+              <div className="user-info">
+                <strong>Bruno Girome</strong>
+                <span>Java, JavaScript, PHP</span>
+              </div>
+            </header>
+            <p>Bio do usuário.</p>
+            <a href="https://github.com/brunogirome">Acessar Perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/44439996?s=460&v=4" alt="avatar" />
+              <div className="user-info">
+                <strong>Bruno Girome</strong>
+                <span>Java, JavaScript, PHP</span>
+              </div>
+            </header>
+            <p>Bio do usuário.</p>
+            <a href="https://github.com/brunogirome">Acessar Perfil no Github</a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
