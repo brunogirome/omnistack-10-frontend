3a4265f079e307d4f9c2859e115cd3b86203bfa9
LN PC <brunogirome@gmail.com>
2020-01-23 18:58:47 -0300

    Alteraçoes do video do mobile

--- src/App.js
+++ src/App.js
@@ -1,78 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from './services/api'
 
 import './global.css'
 import './App.css'
 import './Sidebar.css'
 import './Main.css'
 
+import DevForm from './components/DevForm'
+import DevItem from './components/DevItem'
+
 function App() {
+  const [devs, setDevs] = useState([])
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs')
+
+      setDevs(response.data)
+    }
+
+    loadDevs()
+  }, [])
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data)
+
+    setDevs([...devs, response.data])
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
-            </div>
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
-            </div>
-          </div>
-
-          <button type="submit">
-            Salvar
-          </button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
 
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/44439996?s=460&v=4" alt="avatar" />
-              <div className="user-info">
-                <strong>Bruno Girome</strong>
-                <span>Java, JavaScript, PHP</span>
-              </div>
-            </header>
-            <p>Bio do usuário.</p>
-            <a href="https://github.com/brunogirome">Acessar Perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/44439996?s=460&v=4" alt="avatar" />
-              <div className="user-info">
-                <strong>Bruno Girome</strong>
-                <span>Java, JavaScript, PHP</span>
-              </div>
-            </header>
-            <p>Bio do usuário.</p>
-            <a href="https://github.com/brunogirome">Acessar Perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/44439996?s=460&v=4" alt="avatar" />
-              <div className="user-info">
-                <strong>Bruno Girome</strong>
-                <span>Java, JavaScript, PHP</span>
-              </div>
-            </header>
-            <p>Bio do usuário.</p>
-            <a href="https://github.com/brunogirome">Acessar Perfil no Github</a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
       </main>
     </div>
